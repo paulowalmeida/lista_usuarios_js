801787dfc50e48dd800cc16f856d1f6e6f9dbf2c
Paulo W. A. Ferreira <pauloweskley.dev@gmail.com>
2020-07-26 23:40:20 -0300

    Falta melhorar a implementação da busca.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 let allUsers = null;
+let allUsersFiltered = null;
+let userList = null;
+let userInfo = null;
+let container = null;
 
 function start() {
+  container = document.querySelector(".container");
+  container.classList.add("hide");
+  userList = document.querySelector("#user-list");
+  userInfo = document.querySelector("#users-info");
+
   fetchUsers();
 }
 
@@ -16,6 +25,7 @@ async function fetchUsers() {
       gender,
       email,
       login: { uuid },
+      picture: { medium },
     }) => {
       return {
         id: uuid,
@@ -23,25 +33,73 @@ async function fetchUsers() {
         gender,
         age,
         email,
+        photoURL: medium,
       };
     }
   );
-  console.log(allUsers);
+  allUsers.sort((a, b) => {
+    return a.name.localeCompare(b.name);
+  });
+  allUsersFiltered = [...allUsers];
   render();
 }
 
 function render() {
   renderUsersList();
+  summary();
+  hideLoading();
+  configFilter();
 }
 
 function renderUsersList() {
   let usersHTML = "<div>";
-  allUsers.forEach(({ id, name, gender, age, email }) => {
+  allUsers.forEach(({ id, name, gender, age, email, photoURL }) => {
     const userHTML = `
-      <div id="userCard">
-        
+      <div id="user-item">
+        <div>    
+          <img src="${photoURL}" alt="Foto de Usuário" title="${name}">
+        </div>
+        <div>
+          <ul>
+            <li><h5>${name}, ${age} anos</h5></li>
+            <li>${email}</li>
+          </ul>
+        </div>
       </div>
+
     `;
+    usersHTML += userHTML;
+  });
+  usersHTML += "</div>";
+  userList.innerHTML = usersHTML;
+}
+
+function summary() {}
+
+function hideLoading() {
+  setTimeout(() => {
+    container.classList.remove("hide");
+    const loading = document.querySelector("#loading");
+    loading.classList.add("hide");
+  }, 1000);
+}
+
+function configFilter() {
+  const button = document.querySelector("#button-filter");
+  const inputFilter = document.querySelector("#input-filter");
+  inputFilter.addEventListener("input", (event) => {
+    if (event.target.value === "") {
+      allUsers = [...allUsersFiltered];
+      return;
+    }
+  });
+
+  button.addEventListener("click", () => {
+    const filterValue = inputFilter.value.toLowerCase().trim();
+    allUsers = allUsersFiltered.filter((user) => {
+      return user.name.toLowerCase().includes(filterValue);
+    });
+    render();
   });
 }
 
